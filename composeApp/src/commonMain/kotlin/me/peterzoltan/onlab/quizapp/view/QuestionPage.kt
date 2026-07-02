7be8088a3c2ce6dcb5eed1c644dc954005c98230
peter-zoltan <peter.zoltan2004@gmail.com>
2026-07-02 14:54:47 +0200

    Quiz model no longer directly exposed

--- composeApp/src/commonMain/kotlin/me/peterzoltan/onlab/quizapp/view/QuestionPage.kt
+++ composeApp/src/commonMain/kotlin/me/peterzoltan/onlab/quizapp/view/QuestionPage.kt
@@ -3,14 +3,12 @@ package me.peterzoltan.onlab.quizapp.view
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import me.peterzoltan.onlab.quizapp.viewModel.QuizViewModel
 
 @Composable
 fun QuestionPage(vm: QuizViewModel) {
 
-    val text = vm.getText()
+    val text = vm.text()
 
     Button(
         onClick = { vm.current++ }
--- composeApp/src/commonMain/kotlin/me/peterzoltan/onlab/quizapp/viewModel/QuizViewModel.kt
+++ composeApp/src/commonMain/kotlin/me/peterzoltan/onlab/quizapp/viewModel/QuizViewModel.kt
@@ -7,12 +7,16 @@ import androidx.compose.runtime.setValue
 import me.peterzoltan.onlab.quizapp.model.Question
 import me.peterzoltan.onlab.quizapp.model.Quiz
 
-class QuizViewModel (quiz: Quiz) {
-
-    val quiz by mutableStateOf(quiz)
+class QuizViewModel (val quiz: Quiz) {
 
     var current by mutableStateOf(0)
 
-    fun getText() = quiz.questions[current].text
+    fun text() = quiz.questions[current].text
+
+    fun answers() = ArrayList(quiz.questions[current].answers)
+
+    fun image() = quiz.questions[current].image
+
+    fun time() = quiz.questions[current].time
 
 }
